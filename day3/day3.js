48cce7a5077cd4c48136df01037ff51f08f32f29
dkmoore <dmoore101@gmx.com>
2023-12-02 22:25:42 -0800

    cleaning up day3 code

--- day3/day3.js
+++ day3/day3.js
@@ -8,117 +8,92 @@ const input=readFileAsString("input.txt");
 
 let day3a=function(input){
     lines=input.split('\n');
+
     let M=lines[0].length;
     let N=lines.length-1;
 
+    /* Going for a straight-up state machine parser.
+     * There are M columns of text, plus 1 column of newlines, and 
+     * N rows.
+     * So we can access non-newline characters via input[n*(M+1)+m]
+     * for 0<=m<M and 0<=n<N.
+     * */
 
     let validIndexQ=function(x,y){
         return (x>=0&&y>=0&&x<M&&y<N);
     };
+    /* True if we are any ascii symbol ! through /, : through @, or [ through `, excluding "." */
     let charSymbolQ=function(ch){
         let a=ch.charCodeAt(0);
         return (33<=a&&a<48&&a!=46)||(58<=a&&a<65)||(91<=a && a<97);
     };
+    //True for digits 0-9.
     let charNumberQ=function(ch){
         let a=ch.charCodeAt(0);
         return (48<=a&&a<58);
+    };
+
+    //Check if the character at (x,y) is a symbol with bounds checking
+    let handleSymbolCheck=function(x,y){
+        return validIndexQ(x,y) && charSymbolQ(input[y*(M+1)+x]);
     };
 
     //0==parsing emptiness
     //1==parsing a number, no symbol found yet
     //2==parsing a number, symbol found.
     let parsemode=0;
+    
+    //The current number being parsed
     let number=0;
-    let factor=1; //1, 10, or 100 depending on size of number being added
+
+    //The total sum of part numbers
     let retsum=0;
+
     for(let n=0;n<N;n++){
-        let str="";
         for(let m=0;m<M;m++){
             let ch=input[n*(M+1)+m];
             if(parsemode==0){
                 if(charNumberQ(ch)){
                     number=ch.charCodeAt(0)-48;
                     let adj=[[-1,-1],[-1,0],[-1,1],[0,-1],[0,1]];
-                    let symbolflag=adj.map((pair)=>(
-                        validIndexQ(m+pair[0],n+pair[1])&&charSymbolQ(input[(n+pair[1])*(M+1)+m+pair[0]])
-                    )).reduce((a,b)=>a||b,false);
+                    let symbolflag=adj.map((pair)=>(handleSymbolCheck(m+pair[0],n+pair[1]))).reduce((a,b)=>a||b,false);
                     //If a symbol was found, set parse mode to 2. else 1.
                     if(symbolflag)
                         parsemode=2;
                     else
                         parsemode=1;
                 }
-                //if it's not a number, we don't care.
             } else {
-                //in any case that we find a number, add it to the current 3 digit number parse.
+                //parsemode was already 1 or 2.
+                //If we find a number, add it to the current 3 digit number parse.
                 if(charNumberQ(ch)){
                     number=number*10;
                     number+=ch.charCodeAt(0)-48;
                 }
                 //still have to deal with parsing logic:
                 if(charNumberQ(ch)){
-                    //^ if we find a number, keep parsing.
                     let adj=[[0,-1],[0,1]];
-                    let symbolflag=adj.map((pair)=>(
-                        validIndexQ(m+pair[0],n+pair[1])&&charSymbolQ(input[(n+pair[1])*(M+1)+m+pair[0]])
-                    )).reduce((a,b)=>a||b,false);
+                    let symbolflag=adj.map((pair)=>(handleSymbolCheck(m+pair[0],n+pair[1]))).reduce((a,b)=>a||b,false);
+                    if(symbolflag)
+                        parsemode=2;
+                } else {
+                    let adj=[[0,-1],[0,0],[0,1]];
+                    let symbolflag=adj.map((pair)=>(handleSymbolCheck(m+pair[0],n+pair[1]))).reduce((a,b)=>a||b,false);
                     if(symbolflag){
                         parsemode=2;
                     }
                 }
                 if((!charNumberQ(ch))||m==M-1){
-                    //if it's not a number or we're at the end of the line, do parser wrap up.
-                    let adj=[[0,-1],[0,0],[0,1]];
-                    let symbolflag=adj.map((pair)=>(
-                        validIndexQ(m+pair[0],n+pair[1])&&charSymbolQ(input[(n+pair[1])*(M+1)+m+pair[0]])
-                    )).reduce((a,b)=>a||b,false);
-                    if(symbolflag){
-                        parsemode=2;
-                    }
                     //parser wrap up:
                     if(parsemode===2){
-                        /*
-                        if(n<=5){
-                            console.log("number touching symbol found:"+number);
-                        }*/
                         retsum+=number;
                         number=0;
                     }
                     parsemode=0;
                 }
             }
-
-
-
-
-
-            /*
-            if(charSymbolQ(ch)){
-                str+="s";
-            } else if(charNumberQ(ch)){
-                str+="n";
-            } else {
-                str+=".";
-            }*/
         }
-        //if(n<5)
-            //console.log(str);
     }
-    //n*(M+1)+m
-    /*
-    console.log("nlines: "+lines.length);
-    console.log("nlines[0]: "+lines[0].length);
-    console.log("input: "+input.length);
-
-
-    let sum=0;
-    for(let i=0;i<lines.length;i++){
-        let line=lines[i];
-        if(line.length==0)
-            continue;
-
-    }*/
-
     console.log("=========== day 3 part 1 ==========");
     console.log(retsum);
 };
@@ -246,7 +221,7 @@ let day3b=function(input){
 
     retsum=0;
     for(let i in gearMap){
-        console.log(gearMap[i]);
+        //console.log(gearMap[i]);
         if(gearMap[i].length==2){
             retsum+=gearMap[i][0]*gearMap[i][1];
         }
